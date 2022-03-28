70e78db03b78b29fed48f4fc00f8e7a9514ebd80
BoDee Angus <bodee.angus@gmail.com>
2022-03-28 15:42:29 -0600

    finished implementing header design

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,50 @@
 import React from 'react';
 
-import { Box, Typography } from '@mui/material';
+import MenuIcon from '../../icons/MenuIcon';
+
+import { Box, Typography, IconButton, Grid } from '@mui/material';
 
 export default function Header() {
     return(
         <Box>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography sx={{
-                    color: 'white',
-                    fontWeight: 500,
-                    marginTop: '6px',
-                    marginLeft: '15px',
-                    fontSize: {
-                        xs: '30px'
-                    },
-                    width: {
-                        xs: '193px'
-                    },
-                    lineHeight: {
-                        xs: '30px'
-                    }
-                }}
-                >
-                    Angus Development
-                </Typography>
-                <Box sx={{
-                    height: {
-                        xs: '66px'
-                    },
-                    width: {
-                        xs: '2px'
-                    },
-                    backgroundColor: 'white'
-                }}></Box>
-            </Box>
+            <Grid container>
+                <Grid item xs={8.5}>
+                    <Typography sx={{
+                        color: 'white',
+                        fontWeight: 500,
+                        marginTop: '6px',
+                        marginLeft: '15px',
+                        fontSize: {
+                            xs: '30px'
+                        },
+                        width: {
+                            xs: '193px'
+                        },
+                        lineHeight: {
+                            xs: '30px'
+                        }
+                    }}
+                    >
+                        Angus Development
+                    </Typography>
+                </Grid>
+                <Grid item xs={3.5} sx={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                    <Box sx={{
+                        height: {
+                            xs: '66px'
+                        },
+                        width: {
+                            xs: '2px'
+                        },
+                        backgroundColor: 'white'
+                    }}></Box>
+                    <IconButton sx={{
+                        padding: 0
+                    }}>
+                        <MenuIcon></MenuIcon>
+                    </IconButton>
+                </Grid>
+            </Grid>
             <Box sx={{
                 width: '100%',
                 height: '2px',
---
+++ src/icons/MenuIcon/index.js
@@ -0,0 +1,13 @@
+const MenuIcon = (props) => (
+    <svg
+      width={60}
+      height={11}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path stroke="#fff" strokeWidth={2} d="M0 1h60M0 10h60" />
+    </svg>
+  )
+
+export default MenuIcon  
