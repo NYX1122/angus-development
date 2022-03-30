17152463da40ed18ae2f0db09d14f52c66bd1aea
BoDee Angus <bodee.angus@gmail.com>
2022-03-30 16:19:52 -0600

    added Y-axis responsive design to svg arrows on landing page

--- src/icons/Arrow/index.js
+++ src/icons/Arrow/index.js
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import useWindowDimensions from '../../customHooks/useWindowDimensions';
+
+import { Box } from '@mui/material';
 
 export default function Arrow() {
+    const { height } = useWindowDimensions();
+
+    console.log(height);
+    
+    const lineHeight = () => {
+        if(height < 850) {
+            return 200 - (850 - height);
+        } else {
+            return 200;
+        }
+    }
+
+    const arrowHeight = lineHeight() - 33;
+
     return (
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='61'
-        height='182'
-        fill='none'
-        viewBox='0 0 61 182'
-      >
-        <path stroke='#fff' strokeWidth='3' d='M30.499 180L30.499 0'></path>
-        <path
-          stroke='#fff'
-          strokeWidth='3'
-          d='M30.938 178.939L59.222 150.655'
-        ></path>
-        <path
-          stroke='#fff'
-          strokeWidth='3'
-          d='M0 -1.5L40 -1.5'
-          transform='scale(-1 1) rotate(-45 202.778 125.008)'
-        ></path>
-        <path fill='#fff' d='M30.5 181.488l1.772-1.76H28.73l1.77 1.76z'></path>
-      </svg>
+        <Box sx={{ position: 'relative' }}>
+            <Box>
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='61'
+                    height={`${lineHeight()}px`}
+                    fill='none'
+                    viewBox='0 0 61 182'
+                    preserveAspectRatio='none'
+                >
+                    <path stroke='#fff' strokeWidth='3' d='M30.499 180L30.499 0' vectorEffect='non-scaling-stroke'></path>
+                </svg>
+            </Box>
+            <Box top={arrowHeight} sx={{ position: 'absolute', left: 0 }}>
+                <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='61'
+                height='33'
+                fill='none'
+                viewBox='0 0 61 33'
+                >
+                <path
+                    stroke='#fff'
+                    strokeWidth='3'
+                    d='M30.939 29.939L59.224 1.655'
+                ></path>
+                <path
+                    stroke='#fff'
+                    strokeWidth='3'
+                    d='M0 -1.5L40 -1.5'
+                    transform='scale(-1 1) rotate(-45 22.919 50.51)'
+                ></path>
+                <path fill='#fff' d='M30.502 32.488l1.77-1.76h-3.541l1.771 1.76z'></path>
+                </svg>
+            </Box>
+        </Box>
     );
 }
